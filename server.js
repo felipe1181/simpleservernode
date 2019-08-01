b6933bb0a62e14b80d538185b643cef81dd70233
felipe henrique ferraresi <felip.ses@hotmail.com>
2019-08-01 11:18:22 -0300

    initial commit
    Signed-off-by: felipe henrique ferraresi <felip.ses@hotmail.com>

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.connect("mongodb://192.168.99.100:27017/nodeapi",{useNewUrlParser:true})
+
+app.get('/',function(req,res){
+    res.send('aaaaaai');
+});
+
+app.listen(3001);
+
+
