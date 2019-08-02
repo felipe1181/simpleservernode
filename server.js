c867c3c93fc894c8d3e83bcf785b0c8247acf306
felipe henrique ferraresi <felip.ses@hotmail.com>
2019-08-02 11:50:20 -0300

    creation product model , import model in server.js, and create database with data

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
- 
-
+/**
+ * @author Felipe ferraresi
+ * index server
+ * 
+ * require libs
+*/
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+//open express
 const app = express();
-mongoose.connect("mongodb://192.168.99.100:27017/nodeapi",{useNewUrlParser:true})
+
+//create connection mongodb in docker
+mongoose.connect(
+    "mongodb://192.168.99.100:27017/nodeapi",
+    {useNewUrlParser:true}
+);
+
+//require models 
+requireDir('./src/models');
+const Product = mongoose.model('Product');
+
 
 app.get('/',function(req,res){
-    res.send('aaaaaai');
+    Product.create({
+        title:"NodeJs",
+        description:"Node is a better",
+        url:"www.instagram.com/felipef1181"
+    });
+
+    res.send('Server running');
 });
 
+//open server in 3001 port
 app.listen(3001);
--- src/models/product.js
+++ src/models/product.js
@@ -1,16 +1,30 @@
+/*
+* @author felipe ferraresi
+* create schema mongodb product 
+*/
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     title:{
         type: String,
-        required: true
+        required: true //Required field
     },
     description:{
         type: String,
-        required: true
+        required:true  // Required field
     },
     url:{
         type: String,
         required: true
+    },
+    createAt:{
+        type: Date,
+        default: Date.now
     }
 });
+
+/*
+ * add schema in mongoose
+ */
+
+ mongoose.model('Product',productSchema);
